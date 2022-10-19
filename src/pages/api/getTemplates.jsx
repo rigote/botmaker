ccd49fd86812ab264eb0a8c7e77d616781d38805
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2022-10-19 01:49:13 -0300

    [Fix] - Validando AccessToken

---
+++ src/pages/api/getTemplates.jsx
@@ -0,0 +1,20 @@
+import api from 'api/api'
+
+export default async function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Last-Modified', new Date())
+  res.setHeader('Date', new Date())
+
+  try {
+    const response = await api.get('v1.0/waTemplates', {
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': JSON.stringify(process.env.NEXT_PUBLIC_ACCESSTOKEN)
+      }
+    })
+    res.status(200).json(response)
+  } catch (error) {
+    console.error(error)
+    res.status(error.status || 500).end(error.message)
+  }
+}
