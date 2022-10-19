924a171a92700957f6864e157e062b41d3ceb0c9
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2022-10-19 00:16:29 -0300

    [Fix] - Validando AccessToken

--- src/pages/api/getTemplates.tsx
+++ src/pages/api/getTemplates.tsx
@@ -4,8 +4,9 @@ export default async (req: any, res: any) => {
   try {
     const response = await api.get('v1.0/waTemplates', {
       headers: {
-        'access-token':
+        'access-token': JSON.stringify(
           'eyJhbGciOiJIUzUxMiJ9.eyJidXNpbmVzc0lkIjoiaW5mb2N1c2N4ZGVtbyIsIm5hbWUiOiJUaWFnbyBTYW5jaGVzIiwiYXBpIjp0cnVlLCJpZCI6IjRZV09PNTJRMnRSZndqVDRqN0ZWakNYUDVwSDIiLCJleHAiOjE4MTE5NDEyMzQsImp0aSI6IjRZV09PNTJRMnRSZndqVDRqN0ZWakNYUDVwSDIifQ.0lmyktsqGAtn0jdYdEj--_9yMmwet_mRlcfUkQGXjmODqALcQSnFyhSOAeCY3DHrkp3Lo-E-K_O5cSNQbrr1yg'
+        )
       }
     })
     res.json(response)
