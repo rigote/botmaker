6528428ef07d795dde1a8d2b4784d4c5c5748535
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2022-10-19 00:24:18 -0300

    [Fix] - Validando AccessToken

--- src/pages/api/getTemplates.tsx
+++ src/pages/api/getTemplates.tsx
@@ -1,17 +1,16 @@
 import api from 'api/api'
 
 export default async (req: any, res: any) => {
-  try {
-    const { x_authorization } = req.headers
-
-    if (x_authorization === `Bearer ${process.env.NEXT_PUBLIC_ACCESSTOKEN}`) {
+  const { x_authorization } = req.headers
+  if (
+    x_authorization === `access-token: ${process.env.NEXT_PUBLIC_ACCESSTOKEN}`
+  ) {
+    try {
       const response = await api.get('v1.0/waTemplates')
       res.json(response)
-    } else {
-      res.status(401).end()
+    } catch (error: any) {
+      console.error(error)
+      res.status(error.status || 500).end(error.message)
     }
-  } catch (error: any) {
-    console.error(error)
-    res.status(error.status || 500).end(error.message)
   }
 }
