f70af9980115146612dbef2df9b1af216f0679be
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2022-10-19 02:43:12 -0300

    [Fix] - Validando AccessToken

--- src/pages/api/getTemplates.jsx
+++ src/pages/api/getTemplates.jsx
@@ -2,35 +2,17 @@ import api from 'api/api'
 
 export default async function handler(req, res) {
   res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Last-Modified', new Date())
   res.setHeader('Date', new Date())
 
   try {
-    // const response = await api.get('v1.0/waTemplates', {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'access-token': JSON.stringify(process.env.NEXT_PUBLIC_ACCESSTOKEN)
-    //   }
-    // })
-
-    const response = await fetch(
-      'https://go.botmaker.com/api/v1.0/waTemplates',
-      {
-        headers: {
-          'access-token': JSON.stringify(process.env.NEXT_PUBLIC_ACCESSTOKEN)
-        }
+    const response = await api.get('v1.0/waTemplates', {
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': JSON.stringify(process.env.NEXT_PUBLIC_ACCESSTOKEN)
       }
-    )
-      .then((response) => {
-        console.log(response)
-      })
-      .then((data) => {
-        console.log(data)
-        return data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    })
 
     res.status(200).json(response)
   } catch (error) {
