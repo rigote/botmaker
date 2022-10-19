f5f5f096fba21318fb22c1771e8e7185f1ab3af4
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2022-10-19 01:39:59 -0300

    [Fix] - Validando AccessToken

--- src/pages/api/getTemplates.tsx
+++ src/pages/api/getTemplates.tsx
@@ -6,12 +6,27 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const response = await api.get('v1.0/waTemplates', {
+    // const response = await api.get('v1.0/waTemplates', {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'access-token': JSON.stringify(process.env.NEXT_PUBLIC_ACCESSTOKEN)
+    //   }
+    // })
+    const options = {
+      method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
         'access-token': JSON.stringify(process.env.NEXT_PUBLIC_ACCESSTOKEN)
       }
-    })
+    }
+
+    const response = fetch(
+      'https://go.botmaker.com/api/v1.0/waTemplates',
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err))
+
     res.status(200).json(response)
   } catch (error: any) {
     console.error(error)
