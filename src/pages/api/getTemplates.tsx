33982497ce95c246af8764f5c4a20c7cc52983c2
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2022-10-18 23:48:30 -0300

    [Fix] - Validando AccessToken

--- src/pages/api/getTemplates.tsx
+++ src/pages/api/getTemplates.tsx
@@ -2,13 +2,10 @@ import api from 'api/api'
 
 export default async (req: any, res: any) => {
   try {
-    const response = await api.get('v1.0/waTemplates', {
-      headers: {
-        'access-token':
-          'eyJhbGciOiJIUzUxMiJ9.eyJidXNpbmVzc0lkIjoiaW5mb2N1c2N4ZGVtbyIsIm5hbWUiOiJUaWFnbyBTYW5jaGVzIiwiYXBpIjp0cnVlLCJpZCI6IjRZV09PNTJRMnRSZndqVDRqN0ZWakNYUDVwSDIiLCJleHAiOjE4MTE5NDEyMzQsImp0aSI6IjRZV09PNTJRMnRSZndqVDRqN0ZWakNYUDVwSDIifQ.0lmyktsqGAtn0jdYdEj--_9yMmwet_mRlcfUkQGXjmODqALcQSnFyhSOAeCY3DHrkp3Lo-E-K_O5cSNQbrr1yg'
-      }
-    })
-    res.json(response)
+    // const response = await api.get('v1.0/waTemplates', {
+    //   headers: { 'access-token': process.env.NEXT_PUBLIC_ACCESS_TOKEN }
+    // })
+    res.json(process.env.NEXT_PUBLIC_ACCESS_TOKEN)
   } catch (error: any) {
     console.error(error)
     res.status(error.status || 500).end(error.message)
