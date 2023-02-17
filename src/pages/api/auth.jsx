9a24483bf18c89d56418080aad12365bd571efeb
Matheus Rigote <matheus.rigote@samuraiexperts.com.br>
2023-02-17 00:48:53 -0300

    [Fix] - Get token zendesk

---
+++ src/pages/api/auth.jsx
@@ -0,0 +1,21 @@
+import zen from 'api/zendesk'
+
+export default async function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Last-Modified', new Date())
+  res.setHeader('Date', new Date())
+
+  try {
+    const response = {
+      zen: req.body.zendeskAccessToken,
+      bot: req.body.botmakerAccessToken,
+      chat: req.body.chatChannelNumber
+    }
+
+    res.status(200).json(response.data)
+  } catch (error) {
+    console.error(error)
+    res.status(error.status || 500).end(error.message)
+  }
+}
